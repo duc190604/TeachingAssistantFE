4151b5382bd1c9e6cb40aa4543c1c4467c95e8fe
Tran Thanh Dat <neban0444@gmail.com>
2024-12-22 01:12:15 +0700

    qr code scan to join class

--- app/(studentDetail)/classDetail/addClass.tsx
+++ app/(studentDetail)/classDetail/addClass.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react'
-import { View, Text, TouchableOpacity, SafeAreaView, ScrollView, Alert } from 'react-native'
+import { View, Text, TouchableOpacity, SafeAreaView, ScrollView, Alert, Modal } from 'react-native'
+import { AntDesign, Octicons } from '@expo/vector-icons';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import InputLayout from '@/components/ui/inputLayout';
 import ButtonCustom from '@/components/ui/ButtonCustom';
@@ -9,18 +10,49 @@ import { AuthContext } from '@/context/AuthContext';
 import { ClassSession } from '@/app/(student)/timetable';
 import { useRouter } from 'expo-router';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useCameraPermissions } from 'expo-image-picker';
+import { Camera, CameraView } from "expo-camera";
+
+
+import { Canvas, DiffRect, rect, rrect } from "@shopify/react-native-skia";
+import { Dimensions, Platform, StyleSheet } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+const innerDimension = 300;
+
+const outer = rrect(rect(0, 0, width, height), 0, 0);
+const inner = rrect(
+  rect(
+    width / 2 - innerDimension / 2,
+    height / 2 - innerDimension / 2,
+    innerDimension,
+    innerDimension
+  ),
+  50,
+  50
+);
 
 
 type Props = {}
 
 export default function AddClass({ }: Props) {
+    const [permission, requestPermission] = useCameraPermissions();
+
+    const isPermissionGranted = Boolean(permission?.granted);
+
+    const [modalVisible, setModalVisible] = useState(false)
     const authContext = useContext(AuthContext);
     const router= useRouter();
-  if (!authContext) {
-    Alert.alert("Thông báo", "Đã xảy ra lỗi")
-    return;
-  }
-  const {user,accessToken}= authContext;
+    if (!authContext) {
+      Alert.alert("Thông báo", "Đã xảy ra lỗi")
+      return;
+    } 
+    const qrScanModal = ()=>{
+      return 
+
+    }
+    const {user,accessToken}= authContext;
     const [code, setCode] = useState('')
     const search = async () => {
         const url= `${localHost}/api/v1/subject/join`
@@ -64,8 +96,8 @@ export default function AddClass({ }: Props) {
           </TouchableOpacity>
           <Text className='mx-auto text-[18px] font-msemibold uppercase text-white pr-1'>
             Tham gia lớp học
-          </Text>
-          <TouchableOpacity>
+          </Text> 
+          <TouchableOpacity onPress={()=>setModalVisible(true)}>
             <MaterialCommunityIcons
               name='qrcode-scan'
               size={22}
@@ -86,6 +118,33 @@ export default function AddClass({ }: Props) {
             handle={search}
           />
         </View>
+        <Modal
+                visible={modalVisible}
+                transparent={true}
+                
+                onRequestClose={()=>setModalVisible(false)}
+            >
+              <CameraView
+                style={StyleSheet.absoluteFillObject}
+                facing="back"
+                onBarcodeScanned={({ data }) => {
+                   if (data) {
+                    setCode(data)
+                    setTimeout(async () => {
+                      setModalVisible(false)
+                    }, 500);
+                  }
+                }}
+              />
+              <Canvas
+                style={
+                  Platform.OS === "android" ? { flex: 1 } : StyleSheet.absoluteFillObject
+                }
+              >
+                <DiffRect inner={inner} outer={outer} color="black" opacity={0.5} />
+              </Canvas>
+
+            </Modal>
       </SafeAreaView>
     );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -9,45 +9,45 @@ type Props = {}
 
 export default function index(){
     const authContext = useContext(AuthContext);
-    useEffect(() => {
-        const handleNotification = async () => {
-            messaging().getInitialNotification().then(async (remoteMessage) => {
-                if (remoteMessage) {
-                    console.log('From quit state');
-                    remoteMessage.notification
-                }
-            });   
-            messaging().onNotificationOpenedApp((remoteMessage)=>{
-                console.log('Open from background state');
-                remoteMessage.notification
-            })
-            messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-                console.log('Message handled in the background!', remoteMessage);
-            });
-            messaging().onMessage(async (remoteMessage) => {
-                console.log('Message handled in the foreground!', remoteMessage);
-                if(authContext?.user?.role=='student' && remoteMessage.data?.type=='attendance')
-                    if(remoteMessage.data?.type=='attendance' && remoteMessage.data?.senderId)
-                    {
-                        Alert.alert('Điểm danh ngay', `Môn học: ${remoteMessage.data?.subject}\nPhòng: ${remoteMessage.data?.room}`);
-                    }
-            });
+    // useEffect(() => {
+    //     const handleNotification = async () => {
+    //         messaging().getInitialNotification().then(async (remoteMessage) => {
+    //             if (remoteMessage) {
+    //                 console.log('From quit state');
+    //                 remoteMessage.notification
+    //             }
+    //         });   
+    //         messaging().onNotificationOpenedApp((remoteMessage)=>{
+    //             console.log('Open from background state');
+    //             remoteMessage.notification
+    //         })
+    //         messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    //             console.log('Message handled in the background!', remoteMessage);
+    //         });
+    //         messaging().onMessage(async (remoteMessage) => {
+    //             console.log('Message handled in the foreground!', remoteMessage);
+    //             if(authContext?.user?.role=='student' && remoteMessage.data?.type=='attendance')
+    //                 if(remoteMessage.data?.type=='attendance' && remoteMessage.data?.senderId)
+    //                 {
+    //                     Alert.alert('Điểm danh ngay', `Môn học: ${remoteMessage.data?.subject}\nPhòng: ${remoteMessage.data?.room}`);
+    //                 }
+    //         });
             
-        };
-        const requestUserPermission = async () => {
-            PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
-            const authStatus = await messaging().requestPermission();
-            const enabled =
-                authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-                authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-            if (enabled) {
-                console.log('Authorization status:', authStatus);
-                const token = await messaging().getToken();
-                console.log('FCM token: ',token);
-            }
-        }
-          requestUserPermission();
-          handleNotification();
-    }, [])
+    //     };
+    //     const requestUserPermission = async () => {
+    //         PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+    //         const authStatus = await messaging().requestPermission();
+    //         const enabled =
+    //             authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    //             authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    //         if (enabled) {
+    //             console.log('Authorization status:', authStatus);
+    //             const token = await messaging().getToken();
+    //             console.log('FCM token: ',token);
+    //         }
+    //     }
+    //       requestUserPermission();
+    //       handleNotification();
+    // }, [])
     return <Redirect href='/sign-in'/>
 }
